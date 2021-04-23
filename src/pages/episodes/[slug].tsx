80072a0385c355a673e94b92b57d5b0be5731f90
RafaDom10 <r-domingues@outlook.com>
2021-04-23 16:48:22 -0300

    initial project done

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -1,15 +1,15 @@
-import { GetStaticPaths, GetStaticProps } from 'next';
-import { api } from '../../services/api';
 import { format, parseISO } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
+import { GetStaticPaths, GetStaticProps } from 'next';
+import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
+import React from 'react';
+import { usePlayer } from '../../contexts/PlayerContext';
+import { api } from '../../services/api';
 import { convertDurationToTimeString } from '../../utils/convertDurationToTimeString';
-
 import styles from './episode.module.scss';
-import { useContext } from 'react';
-import { PlayerContext } from '../../contexts/PlayerContext';
+
 
 type Episode = {
   id: string;
@@ -28,10 +28,13 @@ type EpisodeProps = {
 };
 
 export default function Episode({ episode }: EpisodeProps) {
-  const { play } = useContext(PlayerContext);
+  const { play } = usePlayer();
 
   return (
     <div className={styles.episode}>
+      <Head>
+        <title>{episode.title} | Podcastr</title>
+      </Head>
       <div className={styles.thumbnailContainer}>
         <Link href={'/'}>
           <button type="button">
